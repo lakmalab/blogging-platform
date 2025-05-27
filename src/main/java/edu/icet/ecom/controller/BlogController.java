499cb440511cc98bd6a7004a7da7608902d954ef
lakma <lakmalaberathne@gmail.com>
2025-05-27 20:56:17 +0530

    finished the CRUD operations of the backend TODO -Create the frontEnd

--- src/main/java/edu/icet/ecom/controller/BlogController.java
+++ src/main/java/edu/icet/ecom/controller/BlogController.java
@@ -3,10 +3,7 @@ package edu.icet.ecom.controller;
 import edu.icet.ecom.model.dto.BlogDto;
 import edu.icet.ecom.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,15 @@ public class BlogController {
     @PostMapping("/add")
     public void add(@RequestBody BlogDto blogDto) {
         blogService.add(blogDto);
+    }
+
+    @PutMapping
+    public void update(@RequestBody BlogDto blogDto) {
+        blogService.update(blogDto);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        blogService.delete(id);
     }
 
 }
--- src/main/java/edu/icet/ecom/service/BlogService.java
+++ src/main/java/edu/icet/ecom/service/BlogService.java
@@ -67,4 +67,24 @@ public class BlogService {
         return Tags;
     }
 
+    public void update(BlogDto blogDto) {
+        BlogEntity blogEntity = new BlogEntity();
+        if (blogRepository.findById(blogDto.getId()).isPresent()) {
+            blogEntity.setId(blogDto.getId());
+            blogEntity.setTitle(blogDto.getTitle());
+            blogEntity.setContent(blogDto.getContent());
+            blogEntity.setTags(DeformatTags((blogDto.getTags())));
+            blogEntity.setCategory(blogDto.getCategory());
+            blogEntity.setComments_count(blogDto.getComments_count());
+            blogEntity.setCreatedAt(blogDto.getCreatedAt());
+            blogEntity.setUpdated_at(blogDto.getUpdated_at());
+            blogEntity.setImage_url(blogDto.getImage_url());
+
+            blogRepository.save(blogEntity);
+        }
+    }
+
+    public void delete(int id) {
+        blogRepository.deleteById(id);
+    }
 }
